657db1a71678b04f3eb6b5533787af466319b05f
benoit17630 <benoit.menier@orange.fr>
2021-03-22 14:27:20 +0100

    voir datafixture articlefixiture

--- src/Form/Admin/ArticleType.php
+++ src/Form/Admin/ArticleType.php
@@ -8,6 +8,7 @@ use App\Entity\Admin\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,17 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class,[
+                'label'=>"nom",
+
+
+            ])
+            ->add('category', EntityType::class,[
+                "class"=> Category::class,
+                "multiple"=>false,
+                "choice_label"=>"name",
+
+            ])
             ->add('content')
             ->add('isPublished')
             ->add('createdAt',DateType::class,[
