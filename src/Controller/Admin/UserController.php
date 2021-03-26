cdba2b181fcdb366e67ff7af8a913e79b4047e73
benoit17630 <benoit.menier@orange.fr>
2021-03-26 16:08:12 +0100

    creation de l entity user creation de la page d inscription avec envoie d email de verification creation des pages login et logout creation du controller Admin\User pour la gestion des roles

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\Admin\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/admin/user", name="admin_user")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        $users= $userRepository->findAll();
+        return $this->render('admin/user/index.html.twig', [
+            'users'=>$users
+
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/edit/{id}", name="admin_edit" )
+     */
+    public function edit($id,
+                         User $user,
+                         Request $request,
+                         EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+            $manager->persist($user);
+            $manager->flush();
+        }
+        return $this->render("admin/user/edit.html.twig",[
+            'user'=> $user,
+            'form'=>$form->createView()
+        ]);
+    }
+}
---
+++ src/Form/Admin/UserType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class,[
+                "disabled"=> true
+            ])
+            ->add('roles', ChoiceType::class,[
+
+                "choices"=>[
+                    "utilisateur"=>"ROLE_USER",
+                    "administrateur"=>"ROLE_ADMIN"
+                ],
+
+            ])
+            ->add("submit", SubmitType::class,[
+                "label"=>"Modifier"
+            ]);
+
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
